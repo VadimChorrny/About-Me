4bcc17016623dec06f824d3b48796ae18881bd87
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-21 15:04:49 +0300

    init commit

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+// }
+
+import { themes } from '@storybook/theming';
+
+// or global addParameters
+export const parameters = {
+  docs: {
+    theme: themes.dark,
+  },
+};
