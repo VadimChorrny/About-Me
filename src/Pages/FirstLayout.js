9968df4669c5ffa3fdd6b945d3f86857ac2d0efd
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-25 15:03:20 +0200

    fix(layout header) add(fonts)

---
+++ src/Pages/FirstLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../UI/Header'
+
+export default function FirstLayout() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
+
 function Header() {
     return (
         <div className="header">
-            <div className="header__inner" >
+            <div className="header__inner">
                 <div className="header__logo">
                     <p className="header__logo_text">Vadim Chorrny</p>
                 </div>
-                <div className="header__menu">
-
+                <div className="header__burgermenu">
+                    <img src="menu.png" alt="burger-menu" />
                 </div>
             </div>
         </div>
