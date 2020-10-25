71a5b062660e26a8f1c4dc2805584be410eaacc3
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-25 15:29:00 +0200

    add(contents)

---
+++ src/UI/Contents.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Contents() {
+    return (
+        <div className="contents">
+            <div className="contents__inner">
+                <div className="contents__text_first">
+                    <p className="text-first">Full-Stack web Developer</p>
+                </div>
+                <div className="contents__text_second">
+                    <p className="text-second">I create <span>custom</span> and design sites</p>
+                </div>
+                <div className="contents__text_third">
+                    <p className="text-third">A full cycle freelancer who develops and creates great digital products, brands and experiences.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contents
