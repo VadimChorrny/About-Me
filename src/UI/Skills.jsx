024c5409f82e718443a5ff738be3a7f0a5ba219d
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-26 15:17:44 +0200

    add(about-box) | Finish skills layout

--- src/UI/Skills.jsx
+++ src/UI/Skills.jsx
@@ -9,12 +9,31 @@ function Skills() {
                     <p>We are a full-service creative agency, we have a service for all your design, web and marketing needs! World here and now!</p>
                 </div>
                 <div className="skill-box">
-                    <div className="box-first"></div>
-                    <div className="box-first"></div>
-                    <div className="box-first"></div>
+                    <div className="box-first">
+                        <p className="box-logo">DES</p>
+                        <p className="box-design">Design.</p>
+                        <span>Branding</span>
+                        <span>UI/UX Design</span>
+                        <span>Brand Ambassadors</span>
+                    </div>
+                    <div className="box-first">
+                        <p className="box-logo">Bui</p>
+                        <p className="box-design">Build.</p>
+                        <span>Website Development</span>
+                        <span>Web-Applications</span>
+                        <span>Support and Maintenance</span>
+                    </div>
+                    <div className="box-first">
+                        <p className="box-logo">Ma</p>
+                        <p className="box-design">Market.</p>
+                        <span>Didgital Marketing</span>
+                        <span>Video Production</span>
+                        <span>Outsourced Marketing</span>
+                    </div>
 
                 </div>
             </div>
+
         </div>
     )
 }
