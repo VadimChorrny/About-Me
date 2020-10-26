4f10925dc10494f9ec77a352bd2bd0df9e98d7c9
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-26 20:50:15 +0200

    add(footer) finish

--- src/Pages/ThirdLayout.js
+++ src/Pages/ThirdLayout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../UI/About'
+import Footer from '../UI/Footer'
 
 function ThirdLayout() {
     return (
         <div>
             <About />
+            <Footer />
         </div>
     )
 }
